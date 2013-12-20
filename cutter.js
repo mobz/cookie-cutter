22b9287dd4c06a1dcf10ed89d042b31d50f8a05c
Ben Birch <bbirch@aconex.com>
2013-12-20 13:45:46 +1100

    buy cheapest object

--- cutter.js
+++ cutter.js
@@ -12,14 +12,25 @@
 			BuildingsOwned: Game.BuildingsOwned
 		};
 	}
-	function get_cookie() {
+	function buy_cheapest_object() {
+		var cheapest = Game.ObjectsById.sort( function( a, b ) {
+			return a.price - b.price;
+		})[0];
+		if( cheapest.price <= Game.cookies ) {
+			cheapest.buy();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	function click_cookie() {
 		Game.ClickCookie();
 		return true;
 	}
 	clearInterval( window.cutter_timer );
 	window.cutter_timer = setInterval( function() {
 		t++;
-		get_cookie();
+		buy_cheapest_object() || click_cookie();
 		progress();
 	}, 250);
 })();
